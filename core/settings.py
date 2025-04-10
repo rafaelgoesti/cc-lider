6813dd259332a9a90103228c9631c6015054dde7
rafaelti <rafaelduartehd2030@gmail.com>
2025-04-09 21:26:24 -0300

    Corrigido STATIC_ROOT para deploy no Heroku

--- core/settings.py
+++ core/settings.py
@@ -112,6 +112,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
@@ -120,3 +123,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ['*']
